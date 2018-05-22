8cff49ae30833b9d438f821c71756cba89f6fdad
Blaz <blaz.bagic@gmail.com>
2018-05-22 09:56:04 +0200

    Added traceback parsing for python

--- sublime-plugin.py
+++ sublime-plugin.py
@@ -1,10 +1,16 @@
 import sublime
 import sublime_plugin
+import re
 
 
 class SearchStackOverflowCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         panel = sublime.active_window().find_output_panel("exec")
         traceback = panel.substr(sublime.Region(0, panel.size()))
+        error_line = self.parseTraceback(traceback)
         # search_stack_overflow_through_api(traceback)
-        panel.insert(edit, panel.size(), "\nHello, World!")
+        panel.insert(edit, panel.size(), "\n"+error_line)
+
+    def parseTraceback(self, traceback):
+        p = re.compile(r'^(.*Error:.*)$\n\[Finished', re.MULTILINE)
+        return p.findall(traceback)[0]
